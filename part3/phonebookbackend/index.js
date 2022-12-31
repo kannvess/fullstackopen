6b9684addaf5039ea58747ae9c5cde199ce0cdf6
ratob <nashkihaysnairfailfardammahum@gmail.com>
2022-12-31 17:41:14 +0800

    Finished Phonebook backend step8

--- part3/phonebookbackend/index.js
+++ part3/phonebookbackend/index.js
@@ -1,9 +1,13 @@
 const express = require('express')
-const app = express()
 const morgan = require('morgan')
 
+morgan.token('json', (request, response) => {
+	if (request.method === 'POST') return JSON.stringify(request.body)
+})
+
+const app = express()
 app.use(express.json())
-app.use(morgan('tiny'))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :json'))
 
 let persons = [
 	{ 

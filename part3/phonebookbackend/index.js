e097270b7572ea50cc3a0427d859764baa4999ea
ratob <nashkihaysnairfailfardammahum@gmail.com>
2023-01-02 12:49:35 +0800

    Finished phonebook backend step9

--- part3/phonebookbackend/index.js
+++ part3/phonebookbackend/index.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const morgan = require('morgan')
+const app = express()
 
 morgan.token('json', (request, response) => {
 	if (request.method === 'POST') return JSON.stringify(request.body)
 })
 
-const app = express()
+app.use(express.static('build'))
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :json'))
 

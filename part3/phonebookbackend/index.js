31a039de66249065557bb5af73aaa865ad204816
ratob <nashkihaysnairfailfardammahum@gmail.com>
2023-01-03 14:41:40 +0800

    Finished Exercise 3.16: Phonebook database, step4

--- part3/phonebookbackend/index.js
+++ part3/phonebookbackend/index.js
@@ -27,11 +27,12 @@ app.get('/api/persons', (request, response) => {
 	})
 })
 
-app.get('/api/persons/:id', (request, response) => {
-	const id = Number(request.params.id)
-	const person = persons.find(person => person.id === id)
-
-	person ? response.json(person) : response.status(404).end()
+app.get('/api/persons/:id', (request, response, next) => {
+	Person.findById(request.params.id)
+		.then(note => {
+			response.json(note)
+		})
+		.catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
@@ -69,11 +70,13 @@ const errorHandler = (error, request, response, next) => {
 	console.error(error.message)
 
 	if (error.name === 'CastError') {
-		response.status(400).send({error: 'malformatted id'})
+		return response.status(400).send({error: 'malformatted id'})
 	}
-
+``
 	next(error)
 }
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {

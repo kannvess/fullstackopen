55f14fb068a92d34bf72c1ddb84c4fe1eb586c90
ratob <nashkihaysnairfailfardammahum@gmail.com>
2023-01-04 09:48:31 +0800

    Finished Exercise 3.19*: Phonebook database, step7

--- part3/phonebookbackend/index.js
+++ part3/phonebookbackend/index.js
@@ -49,7 +49,7 @@ const generateId = () => {
 	return Math.random() * 9999999999
 }
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
 	const body = request.body
 
 	if (!body.name || !body.number) {
@@ -63,9 +63,11 @@ app.post('/api/persons', (request, response) => {
 		number: body.number
 	})
 
-	person.save().then(savedPerson => {
-		response.json(savedPerson)
-	})
+	person.save()
+		.then(savedPerson => {
+			response.json(savedPerson)
+		})
+		.catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
@@ -88,8 +90,10 @@ const errorHandler = (error, request, response, next) => {
 
 	if (error.name === 'CastError') {
 		return response.status(400).send({error: 'malformatted id'})
+	} else if (error.name === 'ValidationError') {
+		return response.status(400).json({error: error.message})
 	}
-``
+
 	next(error)
 }
 

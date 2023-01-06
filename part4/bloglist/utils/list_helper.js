e761d8a482cba7627ee714efee6d0eb5a80e006a
ratob <nashkihaysnairfailfardammahum@gmail.com>
2023-01-06 10:27:33 +0800

    Finished Exercise 4.4: helper functions and unit tests, step2

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,4 +1,8 @@
 // eslint-disable-next-line no-unused-vars
 const dummy = (blogs) => 1;
 
-module.exports = { dummy };
+const totalLikes = (blogs) => (blogs.length === 0
+  ? 0
+  : blogs.reduce((sum, blog) => sum + blog.likes, 0));
+
+module.exports = { dummy, totalLikes };

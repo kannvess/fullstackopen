9266bdad95b17148efde08e25f382ee0ee29d417
ratob <nashkihaysnairfailfardammahum@gmail.com>
2023-01-17 22:36:43 +0800

    Finished Exercise 5.2-5.3: bloglist frontend, step2-step3

--- part4/bloglist-frontend/src/App.js
+++ part4/bloglist-frontend/src/App.js
@@ -8,23 +8,43 @@ const LoginForm = ({username, setUsername, password, setPassword, handleLogin}) 
     <h2>Log in to application</h2>
     <form onSubmit={handleLogin}>
       <div>
-        username<input type="text" value={username} name="Username" onChange={({target}) => setUsername(target.value)} />
+        username <input type="text" value={username} name="Username" onChange={({target}) => setUsername(target.value)} />
       </div>
       <div>
-        password<input type="password" value={password} name="Password" onChange={({target}) => setPassword(target.value)} />
+        password <input type="password" value={password} name="Password" onChange={({target}) => setPassword(target.value)} />
       </div>
       <button type='submit'>login</button>
     </form>
   </div>
 )
 
-const BlogList = ({blogs, user}) => (
+const UserCredential = ({user, handleLogout}) => (
+  <p>{user.name} logged in <button onClick={handleLogout}>logout</button></p>
+)
+
+const BlogForm = ({title, setTitle, author, setAuthor, url, setUrl, handleNewBlog}) => (
   <div>
-    <h2>blogs</h2>
-    <p>{user.name} logged in</p>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+    <h2>create new</h2>
+    <form onSubmit={handleNewBlog}>
+      <div>
+        title: <input type='text' value={title} name='Title' onChange={({target}) => setTitle(target.value)} />
+      </div>
+      <div>
+        author: <input type='text' value={author} name='Author' onChange={({target}) => setAuthor(target.value)} />
+      </div>
+      <div>
+        url: <input type='text' value={url} name='Url' onChange={({target}) => setUrl(target.value)} />
+      </div>
+      <button type='submit'>create</button>
+    </form>
+  </div>
+)
+
+const BlogList = ({blogs}) => (
+  <div>
+    {blogs.map(blog =>
+      <Blog key={blog.id} blog={blog} />
+    )}
   </div>
 )
 
@@ -33,6 +53,9 @@ const App = () => {
   const [password, setPassword] = useState('')
   const [blogs, setBlogs] = useState([])
   const [user, setUser] = useState(null)
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -40,11 +63,23 @@ const App = () => {
     )  
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
   const handleLogin = async (event) => {
     event.preventDefault()
     const user = await loginService.login({username, password})
 
     try {
+      blogService.setToken(user.token)
+      window.localStorage.setItem('loggedBlogappUser', JSON.stringify(user))
       setUser(user)
       setUsername('')
       setPassword('')
@@ -53,11 +88,39 @@ const App = () => {
     }
   }
 
+  const handleLogout = () => {
+    window.localStorage.removeItem('loggedBlogappUser')
+    setUser(null)
+  }
+
+  const handleNewBlog = (event) => {
+    event.preventDefault()
+    const newBlog = {
+      title: title,
+      author: author,
+      url: url
+    }
+
+    blogService
+      .create(newBlog)
+      .then(returnedBlog => {
+        setBlogs(blogs.concat(returnedBlog))
+        setTitle('')
+        setAuthor('')
+        setUrl('')
+      })
+  }
+
   return (
     <div>
     {user === null
       ? <LoginForm username={username} setUsername={setUsername} password={password} setPassword={setPassword} handleLogin={handleLogin} />
-      : <BlogList blogs={blogs} user={user} />
+      : <div>
+          <h2>blogs</h2>
+          <UserCredential user={user} handleLogout={handleLogout} />
+          <BlogForm title={title} setTitle={setTitle} author={author} setAuthor={setAuthor} url={url} setUrl={setUrl} handleNewBlog={handleNewBlog} />
+          <BlogList blogs={blogs} />
+        </div>
     }
     </div>
   )

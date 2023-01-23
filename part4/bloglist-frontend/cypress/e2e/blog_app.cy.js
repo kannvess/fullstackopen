4ee57967dac0128e941aa2ece5b4f0b451e99f53
ratob <nashkihaysnairfailfardammahum@gmail.com>
2023-01-23 13:34:54 +0800

    Finished Exercise 5.18: bloglist end to end testing, step2 and the bonus exercise

--- part4/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part4/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -1,10 +1,36 @@
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    const user = {
+      name: 'Superuser',
+      username: 'root',
+      password: '123'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users', user)
     cy.visit('http://localhost:3000')
   })
 
   it('Login form is shown', function() {
     cy.contains('log in to application')
+  })
+
+  describe('Login', function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('#username').type('root')
+      cy.get('#password').type('123')
+      cy.contains('login').click()
+
+      cy.contains('blogs')
+    })
+
+    it.only('fails with wrong credentials', function() {
+      cy.get('#username').type('root')
+      cy.get('#password').type('wrong')
+      cy.contains('login').click()
+
+      cy.should('not.contain', 'blogs')
+      cy.get('.error')
+        .should('have.css', 'color', 'rgb(255, 0, 0)')
+    })
   })
 })
--- part4/bloglist-frontend/src/App.js
+++ part4/bloglist-frontend/src/App.js
@@ -9,10 +9,10 @@ const LoginForm = ({ username, setUsername, password, setPassword, handleLogin }
   <div>
     <form onSubmit={handleLogin}>
       <div>
-        username <input type="text" value={username} name="Username" onChange={({ target }) => setUsername(target.value)} />
+        username <input id='username' type="text" value={username} name="Username" onChange={({ target }) => setUsername(target.value)} />
       </div>
       <div>
-        password <input type="password" value={password} name="Password" onChange={({ target }) => setPassword(target.value)} />
+        password <input id='password' type="password" value={password} name="Password" onChange={({ target }) => setPassword(target.value)} />
       </div>
       <button type='submit'>login</button>
     </form>

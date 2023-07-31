7a5968108daa491eec4611f911f6332a42aa5bed
kannvess <kannvess@gmail.com>
2023-07-31 16:32:57 +0900

    Finished studying Part 9b: First steps with TypeScript

--- studying/typescript/partb/calculator.ts
+++ studying/typescript/partb/calculator.ts
@@ -1,24 +1,24 @@
-type Operation = 'multiply' | 'add' | 'divide';
+export type Operation = 'multiply' | 'add' | 'divide';
 // type Result = string | number;
 
-const calculator = (a: number, b: number, op: Operation): number => {
+export const calculator = (a: number, b: number, op: Operation): number => {
   switch (op) {
     case 'add':
       return a + b;
     case 'multiply':
-      return a * b
+      return a * b;
     case 'divide':
       if (b === 0) throw new Error('Can\'t divide by 0!');
       return a / b;
     default:
-      throw new Error('Operation is not multiply, add or divide!')
+      throw new Error('Operation is not multiply, add or divide!');
   }
-}
+};
 
 try {
-  console.log(calculator(1, 5, 'divide'))
+  console.log(calculator(1, 5, 'divide'));
 } catch (error: unknown) {
-  let errorMessage = 'Something went wrong: '
+  let errorMessage = 'Something went wrong: ';
   
   if (error instanceof Error) errorMessage += error.message;
 
--- studying/typescript/partb/multiplier.ts
+++ studying/typescript/partb/multiplier.ts
@@ -11,19 +11,19 @@ const parseArguments = (args: string[]): MultiplyValues => {
     return {
       value1: Number(args[2]),
       value2: Number(args[3])
-    }
+    };
   } else; {
     throw new Error('Provided values were not numbers!');
   }
-}
+};
 
 const multiplier = (a: number, b: number, printText: string) => {
   console.log(printText, a * b);
-}
+};
 
 try {
   const { value1, value2 } = parseArguments(process.argv);
-  multiplier(value1, value2, `Multiplied ${value1} and ${value2}, the result is:`)
+  multiplier(value1, value2, `Multiplied ${value1} and ${value2}, the result is:`);
 } catch (error: unknown) {
   let errorMessage = 'Something bad happened.';
   if (error instanceof Error) {

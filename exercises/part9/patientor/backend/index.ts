2d481ff25a0d16bd452bb03a45dd4ea9bde7e38a
kannvess <kannvess@gmail.com>
2023-09-18 15:27:20 +0700

    Finished exercise 9.8: Patientor backend, step1

---
+++ exercises/part9/patientor/backend/index.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const app = express();
+app.use(express.json());
+
+app.get('/api/ping', (_req, res) => {
+  console.log('Someone pinged here');
+  res.send('pong');
+});
+
+const PORT = 3001;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);  
+});

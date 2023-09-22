9d72eea7411c03d63b9b916eb2e09b5feafe3407
kannvess <kannvess@gmail.com>
2023-09-22 17:30:00 +0700

    Finished studying partc until Exercises 9.10-9.11

--- studying/typescript/partc/src/index.ts
+++ studying/typescript/partc/src/index.ts
@@ -2,12 +2,16 @@ import express from 'express';
 const app = express();
 app.use(express.json());
 
+import diaryRouter from './routes/diaries';
+
 const PORT = 3000;
 
 app.get('/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
 });
+
+app.use('/api/diaries', diaryRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);

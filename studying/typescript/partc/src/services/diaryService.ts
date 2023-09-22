51056bbec828fdea957e1927469d09a272ceb9b3
kannvess <kannvess@gmail.com>
2023-09-22 20:39:38 +0700

    Finished studying part9c until Exercises 9.12-9.13

--- studying/typescript/partc/src/services/diaryService.ts
+++ studying/typescript/partc/src/services/diaryService.ts
@@ -1,6 +1,6 @@
 import diaries from '../../data/diaries';
 
-import { DiaryEntry, NonSensitiveDiaryEntry } from '../types';
+import { DiaryEntry, NewDiaryEntry, NonSensitiveDiaryEntry } from '../types';
 
 const getEntries = (): DiaryEntry[] => {
   return diaries;
@@ -15,12 +15,24 @@ const getNonSensitiveEntries = (): NonSensitiveDiaryEntry[] => {
   }));
 };
 
-const addDiary = () => {
-  return null;
+const addDiary = (entry: NewDiaryEntry): DiaryEntry => {
+  const newDiaryEntry = {
+    id: Math.max(...diaries.map((d) => d.id)) + 1,
+    ...entry,
+  };
+
+  diaries.push(newDiaryEntry);
+  return newDiaryEntry;
+};
+
+const findById = (id: number): DiaryEntry | undefined => {
+  const entry = diaries.find((d) => d.id === id);
+  return entry;
 };
 
 export default {
   getEntries,
   addDiary,
   getNonSensitiveEntries,
+  findById,
 };
--- studying/typescript/partc/src/types.ts
+++ studying/typescript/partc/src/types.ts
@@ -1,6 +1,17 @@
-export type Weather = 'sunny' | 'rainy' | 'cloudy' | 'windy' | 'stormy';
+export enum Weather {
+  Sunny = 'sunny',
+  Rainy = 'rainy',
+  Cloudy = 'cloudy',
+  Stormy = 'stormy',
+  Windy = 'windy',
+}
 
-export type Visibility = 'great' | 'good' | 'ok' | 'poor';
+export enum Visibility {
+  Great = 'great',
+  Good = 'good',
+  Ok = 'ok',
+  Poor = 'poor',
+}
 
 export type NonSensitiveDiaryEntry = Omit<DiaryEntry, 'comment'>;
 
@@ -11,3 +22,5 @@ export interface DiaryEntry {
   visibility: Visibility;
   comment?: string,
 }
+
+export type NewDiaryEntry = Omit<DiaryEntry, 'id'>;

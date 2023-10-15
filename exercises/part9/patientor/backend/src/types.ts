f24577e13c558ebd4b33648ef75db7f37efc0a37
kannvess <kannvess@gmail.com>
2023-10-15 15:34:43 +0800

    Finished 9.22: Patientor, step3

--- exercises/part9/patientor/backend/src/types.ts
+++ exercises/part9/patientor/backend/src/types.ts
@@ -4,14 +4,58 @@ export enum Gender {
   Other = 'other',
 }
 
-export interface DiagnosisEntry {
+export interface Diagnosis {
   code: string;
   name: string;
   latin?: string;
 }
 
-export interface Entry {
+interface Discharge {
+  date: string;
+  criteria: string;
 }
+
+interface SickLeave {
+  startDate: string;
+  endDate: string;
+}
+
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  // diagnosisCodes?: Diagnosis['code'][];
+  diagnosisCodes?: Array<Diagnosis['code']>;
+}
+
+export enum HealthCheckRating {
+  'Healthy' = 0,
+  'LowRisk' = 1,
+  'HighRisk' = 2,
+  'CriticalRisk' = 3,
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: 'HealthCheck';
+  healthCheckRating: HealthCheckRating;
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: 'OccupationalHealthcare';
+  employerName: string;
+  sickLeave?: SickLeave;
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: 'Hospital';
+  discharge: Discharge;
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
 
 export interface PatientEntry {
   id: string;
